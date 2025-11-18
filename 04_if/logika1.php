ad83b68e0bc41420b974366b4a006eb95d1cd60b
athena. <2411102441046@umkt.ac.id>
2025-11-18 14:44:57 +0800

    Delete 04_if directory

--- 04_if/logika1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hobi = "makan";
-if ( ($hobi == "makan") || ($hobi == "ngemil")){
-    echo "Hati-hati, jangan terlalu banyak. <br />";
-    echo "Jangan lupa olahraga ..";
-}
-?>
--- 04_if/tahun_proses.php
+++
@@ -1,9 +0,0 @@
-<?php
-$tahun = intval($_POST['tahun']);
-
-if (($tahun % 4 == 0 && $tahun % 100 != 0) || ($tahun % 400 == 0)) {
-    echo "<p>Tahun ".$tahun." adalah <strong>kabisat</strong>.</p>";
-} else {
-    echo "<p>Tahun ".$tahun." <strong>bukan</strong> tahun kabisat.</p>";
-}
-?>
